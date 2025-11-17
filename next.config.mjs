21c39cd52efa27dbed3fb6c859ecffd723c89138
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-11-17 17:39:23 +0000

    Add config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+import { withPayload } from '@payloadcms/next/withPayload';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	// Your Next.js config here
+	experimental: {
+		reactCompiler: false,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'picsum.photos',
+			},
+		],
+	},
 };
 
-export default nextConfig;
+// Make sure you wrap your `nextConfig`
+// with the `withPayload` plugin
+export default withPayload(nextConfig);
